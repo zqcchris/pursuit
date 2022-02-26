a239a276a29a1d6401b72b523eba1465b3bbccc0
zhaoqingchen <zhaoqingchen@huimei.com>
2022-02-26 15:34:16 +0800

    first commit

--- main.py
+++ main.py
@@ -20,18 +20,3 @@ def pursuit_fill():
 
 
 pursuit_fill()
-
-# data_dic = {
-# 't1':'燕子',
-# 't2':'杨柳',
-# 't3':'桃花',
-# 't4':'针尖',
-# 't5':'头涔涔',
-# 't6':'泪潸潸',
-# 't7':'茫茫然',
-# 't8':'伶伶俐俐',
-# }
-#
-# doc = DocxTemplate('tpl.docx') #加载模板文件
-# doc.render(data_dic) #填充数据
-# doc.save('demo.docx') #保存目标文件
